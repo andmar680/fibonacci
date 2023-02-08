152cf9641fa4c4551ea999088bf57c9360e7c290
andmar680 <andmar680@gmail.com>
2023-02-07 20:32:48 -0600

    first attempt at fibonacci

--- js/fibonacci.js
+++ js/fibonacci.js
@@ -1,5 +1,17 @@
+// returns the nth fibonacci number
+
 const fibonacci = (num) => {
-    
+    if (num < 1) return 'such a number does not exist'
+
+    let fibo = [0, 1];
+    let count = 1
+    while (count !== num) {
+        count++;
+        let cur_sum = fibo[0] + fibo[1];
+        fibo[0] = fibo[1];
+        fibo[1] = cur_sum;
+    }
+    return fibo[0]
 }
 
 module.exports = {fibonacci}
--- js/fibonacciSpec.js
+++ js/fibonacciSpec.js
@@ -1,13 +1,15 @@
 var fib = require("./fibonacci");
 
-console.log(fib.fibonacci(0) === 0)
+console.log(fib.fibonacci(0) === "such a number does not exist") //you can't have a 0th number
 console.log(fib.fibonacci(2) === 1)
-console.log(fib.fibonacci(5) === 5)
-console.log(fib.fibonacci(8) === 21)
-console.log(fib.fibonacci(11) === 89)
-console.log(fib.fibonacci(14) === 377)
-console.log(fib.fibonacci(17) === 1597)
-console.log(fib.fibonacci(20) === 6765)
+console.log(fib.fibonacci(5) === 3)
+console.log(fib.fibonacci(9) === 21)
+console.log(fib.fibonacci(12) === 89)
+console.log(fib.fibonacci(15) === 377)
+console.log(fib.fibonacci(18) === 1597)
+console.log(fib.fibonacci(21) === 6765)
 
-
-
+/*
+n =	0	1	2	3	4	5	6	7	8	9	10	11	12	13	14	...
+xn =	0	1	1	2	3	5	8	13	21	34	55	89	144	233	377	...
+*/
